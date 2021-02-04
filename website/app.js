48664505143a13372d6e2405e11b13e740c4a89d
MartinSchuhmacher <mschuhmacher@gmx.de>
2021-02-04 09:59:34 +0100

    corrected baseURL

--- website/app.js
+++ website/app.js
@@ -1,15 +1,16 @@
 // Personal API Key for OpenWeatherMap API
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 let apiKey = 'a4dc5a01d56a79d90c555f8f638b801b';
 
-// Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', justDoIt);
-
-/* Function called by event listener */
+// Function called by event listener 
 let justDoIt = event => {
     const newZip = document.getElementById('zip-code').value;
     getWeather(baseURL, newZip, apiKey);
 }
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', justDoIt);
+
 
 /* Function to GET Web API Data*/
 const getWeather = async (baseURL, zip, apiKey) => {
